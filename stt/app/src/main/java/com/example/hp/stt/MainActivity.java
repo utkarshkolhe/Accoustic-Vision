6b1879539d1d208cd2483c2c54fc7aaeedf3415e
AYUSHI2016 <AYUSHI2016@users.noreply.github.com>
2018-10-03 12:11:55 +0530

    speech to text simple implementation

---
+++ stt/app/src/main/java/com/example/hp/stt/MainActivity.java
@@ -0,0 +1,126 @@
+package com.example.hp.stt;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    SpeechRecognizer mSpeechRecogniser;
+    Intent mSpeechRecogniserIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        checkPermission();
+        editText=findViewById(R.id.editText);
+        mSpeechRecogniser=SpeechRecognizer.createSpeechRecognizer(this);
+        mSpeechRecogniserIntent=new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        mSpeechRecogniserIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        mSpeechRecogniserIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        mSpeechRecogniser.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int i) {
+
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+                ArrayList<String> matches=bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+                if(matches!=null)
+                {
+                   editText.setText(matches.get(0));
+                }
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+
+            }
+        });
+
+        findViewById(R.id.button).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                switch (motionEvent.getAction()){
+
+                    case MotionEvent.ACTION_UP :
+                        mSpeechRecogniser.stopListening();
+                        editText.setHint("you will see the input");
+                        break;
+                    case MotionEvent.ACTION_DOWN:
+                        editText.setText("");
+                        editText.setHint("listening");
+                        mSpeechRecogniser.startListening(mSpeechRecogniserIntent);
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+    private void checkPermission()
+    {
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+            if(!(ContextCompat.
+                    checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                    == PackageManager.PERMISSION_GRANTED)){
+                Intent intent=new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                        Uri.parse("package:"+getPackageName()));
+                startActivity(intent);
+                finish();
+            }
+        }
+
+    }
+
+}
